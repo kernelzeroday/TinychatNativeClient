82b82cc929d6646b87f54c90b19d112614b8db13
kernelzeroday <enktal@gmail.com>
2018-09-29 03:41:47 -0700

    Add experimental spam detection and prevention

--- tes.js
+++ tes.js
@@ -2099,7 +2099,7 @@ function messageParser() {
 	// this also will fix performance
 	var messageElems = chatlogElem.querySelectorAll(messageQueryString);
 	if (messageElems.length > 100) messageElems[0].parentNode.removeChild(messageElems[0]) ;
-
+	if (latestMessageElem == messageElems[(messageElems.length - 1)]) messageElems[messageElems.length].parentNode.removeChild(messageElems[messageElems.length]);
 	if (latestMessageElem != null) {
 		if (latestMessageElem.classList.contains("system")) typeSystem =  true;
 		latestMessageElem.setAttribute("id", "msg-"+messageCount);
